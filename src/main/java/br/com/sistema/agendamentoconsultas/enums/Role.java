6078a3b9e015d6ceffc84d1b1020c398dd338e98
Elkem Balieiro <elkem.balieiroo@gmail.com>
2023-09-17 09:26:24 -0300

    Refatorando Projeto

--- src/main/java/br/com/sistema/agendamentoconsultas/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package br.com.sistema.agendamentoconsultas.enums;
-
-public enum Role {
-    ROLE_PACIENTE, ROLE_MEDICO, ROLE_ADMIN;
-}
--- src/main/java/br/com/sistema/agendamentoconsultas/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.sistema.agendamentoconsultas.repositories;
-
-import br.com.sistema.agendamentoconsultas.entities.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
